2382ae2d48349ff213c9521542a4af59c9c7513e
DarKCroX <81037822+yukimura-toru@users.noreply.github.com>
2021-06-29 18:26:37 +0530

    Update caption.py

--- plugins/caption.py
+++ plugins/caption.py
@@ -6,10 +6,10 @@ from pyrogram.errors import FloodWait
 
 @Client.on_message(filters.document & filters.channel)
 async def caption(client, message: Message):
-    await message.edit("🤗 Uploaded by @Anime_Wars.\n😄 Group: @Anime_Chat_English. ",
+    await message.edit("🤗 Uploaded by @Anime_Wars.\n😄 Group: @Anime_Chat_English.",
           reply_markup=InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("Channel", url="https://t.me/Anime_Wars)]
+                [InlineKeyboardButton("Channel", url="https://t.me/Anime_Wars")]
             ]
                                            )
                       )
